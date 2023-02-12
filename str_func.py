36fa54e91c72f901c090641bf2772e2c5e3b80f8
alexandr50 <dmitrymaslov2016@mail.ru>
2023-02-12 12:04:15 +0300

    change str_func.py

--- str_func.py
+++ str_func.py
@@ -1,3 +1,11 @@
 def get_upper(text: str):
+    """Функция преобразующая текст в заглавные буквы"""
     return text.upper()
+
+
+def get_tilte(text: str):
+    """ Функция преобразующая первые буквы слова  в заглавные"""
+
+    return ' '.join([x.title() for x in text.split(' ')])
+
 
